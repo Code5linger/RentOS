9e6e4251c1c0f70fe91fb36e83c68ddddf995a53
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2026-04-23 18:28:22 +0600

    !

--- prisma.config.ts
+++ prisma.config.ts
@@ -18,13 +18,9 @@ import { defineConfig } from 'prisma/config';
 //   },
 // });
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not defined in environment variables');
-}
-
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || 'postgresql://dummy:dummy@localhost:5432/dummy',
   },
 });
